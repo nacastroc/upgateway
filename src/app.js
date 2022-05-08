679d9a9dc4df2c7b5418c490883af9ebc9a9916e
Nestor <nacastroc@gmail.com>
2022-05-08 01:17:34 -0400

    Add response headers

--- src/app.js
+++ src/app.js
@@ -15,17 +15,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, Content-Length, Authorization, Accept, X-Requested-With')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Max-Age', '86400')    
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/api/gateways', gatewaysRouter);
 app.use('/api/peripherals', peripheralsRouter);
 
 app.use(function (req, res, next) {
     res.sendStatus(404);
-})
+});
 
 app.use(function (err, req, res, next) {
     res.status(500);
     res.send({ message: err.message, stack: err.stack });
-})
+});
 
 module.exports = app;
